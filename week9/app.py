5b01618e9274365d62477505a210fb02baa52df5
Toms <tomsterauds@gmail.com>
2022-09-09 17:04:51 +0300

    lecture9

--- week9/app.py
+++ week9/app.py
@@ -1,8 +1,24 @@
-from flask import Flask, render_template, request
+from crypt import methods
+from re import X
+from unicodedata import name
+from flask import Flask, render_template, request 
 
 app = Flask(__name__)
 
-@app.route("/")
 
+
+@app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/sveicinats", methods=['POST'])
+def atbilde():
+    x = request.args.get("q", "janis") # input janis if empty form
+    y = request.args.get("qq", "jansons") # --``--
+    z = request.form.get("p","password")
+    return render_template("sveiciens.html", pirmais = x, otrais = y, parole = z)
+
+
+#if __name__=='__main__':
+app.run()
