acb498e0f269a94fb1d082b89f2ae221d50279c2
Toms <tomsterauds@gmail.com>
2022-09-09 20:47:57 +0300

    added jinja loop

--- week9/froshims/app.py
+++ week9/froshims/app.py
@@ -2,16 +2,23 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+ALPHA = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "e"
+]
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", alpha=ALPHA)
 
 @app.route("/reg..", methods=["POST"])
 def reg():
 
     # validate submission
-    if not request.form.get("x") or request.form.get("y") not in ["A", "B", "C"]:
+    if not request.form.get("x") or request.form.get("y") not in ALPHA:
         return render_template("failure.html")
     
     # Confirm registration
