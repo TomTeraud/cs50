b6d5a3a84c842524c250129159690e73c3eb0e01
Toms <tomsterauds@gmail.com>
2022-09-10 22:46:24 +0300

    added data storage from form

--- week9/flaskWithDb/app.py
+++ week9/flaskWithDb/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -14,3 +14,17 @@ FRUITS = [
 @app.route('/')
 def index():
     return render_template("index.html", augli = FRUITS)
+
+@app.route("/parbaude", methods=['POST'])
+def ievade():
+    vards = request.form.get('vards')
+    
+    auglis = request.form.get('auglis')
+    KATALOGS[vards] = auglis
+
+
+    return redirect("/db")
+
+@app.route("/db")
+def izeja():
+    return render_template("izeja.html", iz = KATALOGS)
